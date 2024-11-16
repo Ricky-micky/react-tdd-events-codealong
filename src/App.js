04971c1a9253198847e367027bc1a9a4c8577354
Ricky-micky <erick.tembo@student.moringaschool.com>
2024-11-16 16:49:53 +0300

    add text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 function App() {
   return (
-    <div></div>
+    <div>
+       <h2>Lorem Ipsum</h2>
+      
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mollis pharetra volutpat. 
+        In eu sapien purus. Phasellus laoreet, lorem id lobortis faucibus, sapien lacus rhoncus enim, ut accumsan enim lorem ut arcu.
+        Aliquam aliquam massa a vestibulum vulputate. Vestibulum lacinia risus a ante mollis, in rutrum diam egestas. 
+        Ut lobortis risus id sapien rhoncus, id hendrerit sem molestie.
+        Vivamus justo dui, aliquet vel sodales ac, tincidunt eget libero. </p>
+    </div>
   )
 }
 
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,20 @@ import App from "../App";
 import '@testing-library/jest-dom';
 
 // Code tests here
+
+
+// import { render, screen } from "@testing-library/react";
+// import App from "../App";
+
+import '@testing-library/jest-dom';
+
+// Code tests here
+
+test ('renders a header and a paragraph', () => {
+
+    render (<App />)
+
+    // test header 
+    const pageHeader = screen.getByText (/Lorem Ipsum/)
+
+});
